196a16989262fba447c53cfa35c063ecddd39ea4
Lynda Trad <lynda.trad@etu.u-pec.fr>
2020-03-04 22:04:00 +0100

    Delete IChatLogger.java

--- IChatLogger.java
+++
@@ -1,8 +0,0 @@
-public interface IChatLogger 
-{	
-	public void clientConnected(String ip);
-	public void clientDisconnected(String ip, String name);
-	public void clientGotName(String ip, String name);
-	public void clientGotCommand(String name, int command);
-	public void systemMessage(String message);
-}
